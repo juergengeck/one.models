b2f92451383797ca3439ade0b4cabf8c4999167b
sandrusebastian <sebastian.sandru@corandus.com>
2020-06-16 16:19:19 +0300

    Added support for one line import for models

---
+++ src/model/index.ts
@@ -0,0 +1,34 @@
+import JournalModel from './JournalModel';
+import QuestionnaireModel from './QuestionnaireModel';
+import WbcDiffModel from './WbcDiffModel';
+import HeartEventModel from './HeartEventModel';
+import DocumentModel from './DocumentModel';
+import ConnectionsModel from './ConnectionsModel';
+import BodyTemperatureModel from './BodyTemperatureModel';
+import DiaryModel from './DiaryModel';
+import NewsModel from './NewsModel';
+import OneInstanceModel, {AuthenticationState} from './OneInstanceModel';
+import ChannelManager from './ChannelManager';
+import ContactModel from './ContactModel';
+import ConsentFileModel from './ConsentFileModel';
+import PropertyTreeStore, {PropertyTree} from './SettingsModel';
+
+/** for one line imports **/
+export {
+    JournalModel,
+    QuestionnaireModel,
+    WbcDiffModel,
+    HeartEventModel,
+    DocumentModel,
+    ConnectionsModel,
+    BodyTemperatureModel,
+    DiaryModel,
+    NewsModel,
+    OneInstanceModel,
+    AuthenticationState,
+    ChannelManager,
+    ContactModel,
+    ConsentFileModel,
+    PropertyTreeStore,
+    PropertyTree
+};
