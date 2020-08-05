eeeeb7d65b38a08267233530a588c3b02bfe08ad
Oana Gugea <oana.gugea@corandus.com>
2020-08-05 11:20:59 +0300

    post to channel fix

--- plans/postToChannel.ts
+++ plans/postToChannel.ts
@@ -1,10 +1,16 @@
-import {getObject, getObjectByIdHash, VersionedObjectResult, WriteStorageApi} from 'one.core/lib/storage';
+import {
+    getObject,
+    getObjectByIdHash,
+    VersionedObjectResult,
+    WriteStorageApi
+} from 'one.core/lib/storage';
 import {
     OneUnversionedObjectTypes,
     SHA256IdHash,
     UnversionedObjectResult,
     ChannelEntry,
-    ChannelInfo, Person
+    ChannelInfo,
+    Person
 } from '@OneCoreTypes';
 import {calculateHashOfObj, calculateIdHashOfObj} from 'one.core/lib/util/object';
 
@@ -12,28 +18,28 @@ import {calculateHashOfObj, calculateIdHashOfObj} from 'one.core/lib/util/object
  * Create a new questionnaire entry in the passed channel
  *
  * @param {WriteStorageApi} WriteStorage
- * @param {string} channelid
+ * @param {string} channelId
  * @param {SHA256IdHash<Person>} owner
  * @param {OneUnversionedObjectTypes} payload
  * @returns {Promise<VersionedObjectResult<ChannelInfo>>}
  */
 export async function createObjects(
     WriteStorage: WriteStorageApi,
-    channelid: string,
+    channelId: string,
     owner: SHA256IdHash<Person>,
     payload: OneUnversionedObjectTypes
 ): Promise<VersionedObjectResult<ChannelInfo>> {
     // Get the ChannelInfo from the database
     const channelInfoIdHash: SHA256IdHash<ChannelInfo> = await calculateIdHashOfObj({
         $type$: 'ChannelInfo',
-        id: channelid,
+        id: channelId,
         owner: owner
     });
     const channelInfoResult = await getObjectByIdHash<ChannelInfo>(channelInfoIdHash);
 
     const payloadHash = await calculateHashOfObj(payload);
     try {
-        await getObject(payloadHash)
+        await getObject(payloadHash);
     } catch (e) {
         await WriteStorage.storeUnversionedObject(payload);
     }
@@ -58,7 +64,7 @@ export async function createObjects(
     // Update the head of the ChannelInfo entry
     return await WriteStorage.storeVersionedObject({
         $type$: 'ChannelInfo',
-        id: channelid,
+        id: channelId,
         owner: owner,
         head: channelEntryResult.hash
     });
