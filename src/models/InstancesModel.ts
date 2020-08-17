d8cb52eaa45d9faafbee87c077030f9e45482ed0
Erik Haßlmeyer <erik@refinio.net>
2020-08-17 13:43:36 +0200

    Cleaned up the CommunicationModule and InstanceModule

--- src/models/InstancesModel.ts
+++ src/models/InstancesModel.ts
@@ -12,11 +12,49 @@ import {getAllValues} from 'one.core/lib/reverse-map-query';
 import {calculateIdHashOfObj} from 'one.core/lib/util/object';
 import {createRandomString} from 'one.core/lib/system/crypto-helpers';
 import {serializeWithType} from 'one.core/lib/util/promise';
-import {authenticateOwner, loadInstanceKeys} from 'one.core/lib/instance-crypto';
+import {
+    authenticateOwner,
+    createCrypto,
+    CryptoAPI,
+    loadInstanceKeys
+} from 'one.core/lib/instance-crypto';
 import {EventEmitter} from 'events';
 
 /**
- * This model manages all of the instance objects of everybody.
+ * This type stores information about an instance.
+ */
+export type LocalInstanceInfo = {
+    isMain: boolean;
+    personId: SHA256IdHash<Person>; // Id of person
+    instanceId: SHA256IdHash<Instance>; // Id of corresponding local instance
+    instanceKeys: Keys; // Keys of corresponding local instance
+    cryptoApi: CryptoAPI; // Crypto api
+};
+
+/**
+ * This model manages all of the instance objects.
+ *
+ * At the moment it focuses on managing all local instance.
+ * A local instance is an instance object that was created locally, so has private keys.
+ * Usually each identity of myself should have exactly one local instance.
+ *
+ * The methods can be grouped into multiple categories:
+ * - query information about all local instances at once
+ *   -> localInstances* Methods
+ * - query information about a specific local instance (based on instance id)
+ *   -> localInstance*
+ * - query information about a specific local instance (based on person id)
+ *   -> localInstance*ForPerson methods
+ * - query information about the main local instance (the local instance whose owner is the main id)
+ *   -> mainInstance* methods
+ * - add new local instances
+ *   -> the rest
+ *
+ * The * is mostly one of those:
+ * - <nothing>: Returns instance object(s)
+ * - Id: Returns instance hash id(s)
+ * - Keys: Returns Key object(s)
+ * - Info: Returns LocalInstanceInfo object(s)
  */
 class InstancesModel extends EventEmitter {
     private secret: string = '';
@@ -63,27 +101,7 @@ class InstancesModel extends EventEmitter {
         );
     }
 
-    /**
-     * Returns your main instance object.
-     *
-     * @returns {Promise<Instance>}
-     */
-    public async mainInstance(): Promise<Instance> {
-        return (await getObjectByIdHash(await this.mainInstanceId())).obj;
-    }
-
-    /**
-     * Returns the id of your main instance.
-     *
-     * @returns {Promise<SHA256IdHash<Instance>>}
-     */
-    public async mainInstanceId(): Promise<SHA256IdHash<Instance>> {
-        const idHash = await getInstanceIdHash();
-        if (!idHash) {
-            throw new Error('There is no instance id hash.');
-        }
-        return idHash;
-    }
+    // ######## Generic query methods for all local instances ########
 
     /**
      * Returns all instance objects that represent your local instance.
@@ -132,6 +150,126 @@ class InstancesModel extends EventEmitter {
     }
 
     /**
+     * Returns a list of info objects for all local instances.
+     *
+     * @param {boolean} exclude_main
+     * @param {LocalInstancesList} localInstancesList
+     * @returns {Promise<LocalInstanceInfo[]>}
+     */
+    public async localInstancesInfo(
+        exclude_main: boolean = false,
+        localInstancesList?: LocalInstancesList
+    ): Promise<LocalInstanceInfo[]> {
+        const ids = await this.localInstancesIds();
+        return Promise.all(ids.map(id => this.localInstanceInfo(id)));
+    }
+
+    // ######## Instance query methods for main instance ########
+
+    /**
+     * Returns your main instance object.
+     *
+     * @returns {Promise<Instance>}
+     */
+    public async mainInstance(): Promise<Instance> {
+        return (await getObjectByIdHash(await this.mainInstanceId())).obj;
+    }
+
+    /**
+     * Returns the id of your main instance.
+     *
+     * @returns {Promise<SHA256IdHash<Instance>>}
+     */
+    public async mainInstanceId(): Promise<SHA256IdHash<Instance>> {
+        const idHash = await getInstanceIdHash();
+        if (!idHash) {
+            throw new Error('There is no instance id hash.');
+        }
+        return idHash;
+    }
+
+    /**
+     * Get the main instance keys.
+     *
+     * @returns {Promise<Keys>}
+     */
+    public async mainInstanceKeys(): Promise<Keys> {
+        return this.localInstanceKeys(await this.mainInstanceId());
+    }
+
+    /**
+     * Returns multiple information of your main instance.
+     *
+     * @returns {Promise<LocalInstanceInfo>}
+     */
+    public async mainInstanceInfo(): Promise<LocalInstanceInfo> {
+        return this.localInstanceInfo(await this.mainInstanceId());
+    }
+
+    // ######## Instance query methods based on instance ID ########
+
+    /**
+     * Ge tthe
+     * @param {SHA256IdHash<Instance>} instanceId
+     * @returns {Promise<Instance>}
+     */
+    public async localInstance(instanceId: SHA256IdHash<Instance>): Promise<Instance> {
+        if (!(await this.isLocalInstance(instanceId))) {
+            throw new Error('Passed instance is not a local instance');
+        }
+        return (await getObjectByIdHash(instanceId)).obj;
+    }
+
+    /**
+     * Get the instance keys for a specific person.
+     *
+     * @param {SHA256IdHash<Person>} personId
+     * @returns {Promise<Keys>}
+     */
+    public async localInstanceKeys(instanceId: SHA256IdHash<Instance>): Promise<Keys> {
+        if (!(await this.isLocalInstance(instanceId))) {
+            throw new Error('Passed instance is not a local instance');
+        }
+        const instanceKeyLink = await getAllValues(instanceId, true, 'Keys');
+        return await getObjectWithType(instanceKeyLink[instanceKeyLink.length - 1].toHash, 'Keys');
+    }
+
+    /**
+     * Obtain the instance info for a certain locale instance.
+     *
+     * @param {SHA256IdHash<Instance>} instanceId
+     * @returns {Promise<LocalInstanceInfo>}
+     */
+    public async localInstanceInfo(instanceId: SHA256IdHash<Instance>): Promise<LocalInstanceInfo> {
+        if (!(await this.isLocalInstance(instanceId))) {
+            throw new Error('Passed instance is not a local instance');
+        }
+        const instance = await getObjectByIdHash(instanceId);
+        const instanceKeys = await this.localInstanceKeys(instanceId);
+        const cryptoApi = createCrypto(instanceId);
+        return {
+            isMain: instanceId === (await this.mainInstanceId()),
+            personId: instance.obj.owner,
+            instanceId,
+            instanceKeys,
+            cryptoApi
+        };
+    }
+
+    /**
+     * Checks whether the instance is local.
+     *
+     * @param {SHA256IdHash<Instance>} instanceId
+     * @returns {Promise<boolean>}
+     */
+    public async isLocalInstance(instanceId: SHA256IdHash<Instance>): Promise<boolean> {
+        const list = await this.localInstancesIds();
+        return list.find(instances => instances === instanceId) !== undefined;
+    }
+
+    // ######## Instance query method based on Person ID ########
+
+    /**
      * Get the local instance for a specific person.
      *
      * @param {SHA256IdHash<Person>} personId
@@ -172,19 +310,20 @@ class InstancesModel extends EventEmitter {
      * @param {SHA256IdHash<Person>} personId
      * @returns {Promise<Keys>}
      */
-    public async instanceKeysForPerson(personId: SHA256IdHash<Person>): Promise<Keys> {
-        return await this.instanceKeys(await this.localInstanceIdForPerson(personId));
+    public async localInstanceKeysForPerson(personId: SHA256IdHash<Person>): Promise<Keys> {
+        return await this.localInstanceKeys(await this.localInstanceIdForPerson(personId));
     }
 
     /**
-     * Get the instance keys for a specific person.
+     * Get the instance info related to the local info of the passed person
      *
      * @param {SHA256IdHash<Person>} personId
-     * @returns {Promise<Keys>}
+     * @returns {Promise<LocalInstanceInfo>}
      */
-    public async instanceKeys(instanceId: SHA256IdHash<Instance>): Promise<Keys> {
-        const instanceKeyLink = await getAllValues(instanceId, true, 'Keys');
-        return await getObjectWithType(instanceKeyLink[instanceKeyLink.length - 1].toHash, 'Keys');
+    public async localInstanceInfoForPerson(
+        personId: SHA256IdHash<Person>
+    ): Promise<LocalInstanceInfo> {
+        return await this.localInstanceInfo(await this.localInstanceIdForPerson(personId));
     }
 
     /**
@@ -201,6 +340,8 @@ class InstancesModel extends EventEmitter {
         const instanceResult = instanceResults.find(instance => instance.obj.owner === personId);
         return instanceResult !== undefined;
     }
+
+    // ######## Modify local instances list / create ... ########
 
     /**
      * Creates a local instance for the specified owner.
