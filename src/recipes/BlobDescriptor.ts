7fb1ef6983c8caa253c446eb251ddb29c0f18f1a
Maximilian Wisgickl <wisgicklma@protonmail.com>
2020-09-01 12:14:19 +0200

    Added BlobCollection recipe and rename BlobDescriptor props to the same used in Blob and File from MDN

--- src/recipes/BlobDescriptor.ts
+++ src/recipes/BlobDescriptor.ts
@@ -2,15 +2,24 @@
 declare module '@OneCoreTypes' {
 
     export interface OneUnversionedObjectInterfaces {
+        BlobCollection: BlobCollection;
         BlobDescriptor: BlobDescriptor;
+    }
+
+    export interface BlobCollection {
+        $type$: 'BlobCollection';
+        name: string;
+        blobs: BlobDescriptor[];
     }
 
     export interface BlobDescriptor {
         $type$: 'BlobDescriptor';
-        name: string;
-        sizeInBytes: number;
-        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#Types
-        mimeType: string;
         data: SHA256Hash<BLOB>;
+        // unixtimestamp
+        lastModified: number;
+        name: string;
+        size: number;
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#Types
+        type: string;
     }
 }
