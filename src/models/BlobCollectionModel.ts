17044164576d06d003bf574c315547fcb0dd462e
Maximilian Wisgickl <wisgicklma@protonmail.com>
2020-09-02 08:11:15 +0200

    Changed things

--- src/models/BlobCollectionModel.ts
+++ src/models/BlobCollectionModel.ts
@@ -1,17 +1,33 @@
 import EventEmitter from 'events';
-import {BlobCollection} from '@OneCoreTypes';
-import ChannelManager from './ChannelManager';
+import {BlobCollection, Person, SHA256IdHash} from '@OneCoreTypes';
+import ChannelManager, {ObjectData} from './ChannelManager';
 import {createSingleObjectThroughPurePlan} from 'one.core/lib/storage';
+import ContactModel from './ContactModel';
 
 export default class BlobCollectionModel extends EventEmitter {
-    private collections: BlobCollection[] = [];
     channelManager: ChannelManager;
+    contactManager: ContactModel;
+    owner: SHA256IdHash<Person> | undefined;
     channelId = 'blobCollections';
 
-    constructor(channelManager: ChannelManager) {
+    constructor(channelManager: ChannelManager, contactModel: ContactModel) {
         super();
 
         this.channelManager = channelManager;
+        this.contactManager = contactModel;
+    }
+
+    /**
+     * Used to init the model to receive the updates.
+     */
+    async init() {
+        await this.channelManager.createChannel(this.channelId);
+        this.owner = await this.contactManager.myMainIdentity();
+        this.channelManager.on('updated', id => {
+            if (id === this.channelId) {
+                this.emit('updated');
+            }
+        });
     }
 
     async addCollection(files: File[], name: string): Promise<void> {
@@ -20,25 +36,40 @@ export default class BlobCollectionModel extends EventEmitter {
             files,
             name
         );
+
+        await this.channelManager.postToChannel(this.channelId, blobCollection.obj);
     }
 
-    getCollection(name: BlobCollection['name']): BlobCollection {
-        const collection = this.collections.find(collection => collection.name === name);
-
+    async getCollection(name: BlobCollection['name']): Promise<BlobCollection> {
+        const collections = await this.channelManager.getObjectsWithType(
+            this.channelId,
+            'BlobCollection',
+            {owner: this.owner}
+        );
+        const collection = collections.find(
+            (objectData: ObjectData<BlobCollection>) => objectData.data.name === name
+        );
         if (collection) {
-            return collection;
+            return collection.data;
         } else {
             throw new Error(`BlobCollection ${name} not found.`);
         }
     }
 
-    getLatestCollection(): BlobCollection {
-        const collection = this.collections[this.collections.length - 1];
-
+    async getLatestCollection(): Promise<BlobCollection> {
+        const collection = await this.channelManager.getObjectsWithType(
+            this.channelId,
+            'BlobCollection',
+            {
+                count: 1,
+                owner: this.owner
+            }
+        );
         if (collection) {
-            return collection;
+            // todo change return value, load all blobs as arrayBuffers
+            return collection[0].data;
         } else {
-            throw new Error(`No collections were found.`);
+            throw new Error(`BlobCollection ${name} not found.`);
         }
     }
 }
