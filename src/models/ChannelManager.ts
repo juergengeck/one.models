384f30372393bc39034564d98fd09d5ddce59d0a
Erik Haßlmeyer <erik@refinio.net>
2020-09-14 15:51:20 +0200

    Fixed passing variable name as string instead of variable

--- src/models/ChannelManager.ts
+++ src/models/ChannelManager.ts
@@ -218,7 +218,7 @@ export default class ChannelManager extends EventEmitter {
     ): Promise<void> {
         // Iterate over the channel to see whether the object exists.
         const dataHash = await calculateHashOfObj(data);
-        for await (const item of this.objectIterator('channelId', {
+        for await (const item of this.objectIterator(channelId, {
             owner: owner ? owner : this.personId
         })) {
             if ((await calculateHashOfObj(item.data)) === dataHash) {
