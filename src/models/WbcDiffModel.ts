d6f67447852bbbf168dc0805444b5ea1adaa2c32
Maximilian Wisgickl <wisgicklma@protonmail.com>
2020-10-19 09:57:07 +0200

    Adjust WbcDiffRecipes.ts and WbcDiffModel.ts

--- src/models/WbcDiffModel.ts
+++ src/models/WbcDiffModel.ts
@@ -50,115 +50,40 @@ export default class WbcDiffModel extends EventEmitter {
     /**
      * Create a new response for a questionnaire.
      *
-     * @param {string} wbcMeasurement - The answers for the questionnaire
+     * @param {string} wbcObservation - The answers for the questionnaire
      */
-    async postMeasurement(wbcMeasurement: WbcObservation): Promise<void> {
-        wbcMeasurement = Object.assign({}, wbcMeasurement); // shallow copy, because we modify it
+    async postMeasurement(wbcObservation: WbcObservation): Promise<void> {
+        wbcObservation = Object.assign({}, wbcObservation); // shallow copy, because we modify it
 
         // Verify number format of *Count fields
         const numberRegex = /^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/;
 
-        if (!numberRegex.test(wbcMeasurement.wbcCount)) {
-            throw Error('The wbcCount number has wrong format.');
-        }
-
-        if (wbcMeasurement.neuCount !== undefined) {
-            if (!numberRegex.test(wbcMeasurement.neuCount)) {
-                throw Error('The neuCount field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.lymCount !== undefined) {
-            if (!numberRegex.test(wbcMeasurement.lymCount)) {
-                throw Error('The lymCount field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.monCount !== undefined) {
-            if (!numberRegex.test(wbcMeasurement.monCount)) {
-                throw Error('The monCount field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.eosCount !== undefined) {
-            if (!numberRegex.test(wbcMeasurement.eosCount)) {
-                throw Error('The eosCount field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.basCount !== undefined) {
-            if (!numberRegex.test(wbcMeasurement.basCount)) {
-                throw Error('The basCount field has wrong format.');
-            }
-        }
-
-        // Verify the supported units(for now verifies 10^9/dL or 1000000000/dL both formats)
-        // TODO: Verify the units when they are clear!
-        const unitRegex = /^(1000000000)(\/)(dL)|^(10)(\^)(9)(\/)(dL)$/;
-
-        if (!unitRegex.test(wbcMeasurement.wbcCountUnit)) {
-            throw Error('The wbcCountUnit number has wrong format.');
-        }
-
-        if (wbcMeasurement.neuCountUnit !== undefined) {
-            if (!unitRegex.test(wbcMeasurement.neuCountUnit)) {
-                throw Error('The neuCountUnit field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.lymCountUnit !== undefined) {
-            if (!unitRegex.test(wbcMeasurement.lymCountUnit)) {
-                throw Error('The lymCountUnit field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.monCountUnit !== undefined) {
-            if (!unitRegex.test(wbcMeasurement.monCountUnit)) {
-                throw Error('The monCountUnit field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.eosCountUnit !== undefined) {
-            if (!unitRegex.test(wbcMeasurement.eosCountUnit)) {
-                throw Error('The eosCountUnit field has wrong format.');
-            }
-        }
-
-        if (wbcMeasurement.basCountUnit !== undefined) {
-            if (!unitRegex.test(wbcMeasurement.basCountUnit)) {
-                throw Error('The basCountUnit field has wrong format.');
+        let property: keyof WbcObservation;
+        for (property in wbcObservation) {
+            if (property !== 'acquisitonTime' && property !== '$type$') {
+                const stringNumber = wbcObservation[property].value;
+                if (!numberRegex.test(stringNumber)) {
+                    throw new Error(`${stringNumber} of ${property} is not a valid Number format`);
+                }
             }
         }
 
         // post wbc measurement to channel
-        await this.channelManager.postToChannel(this.channelId, wbcMeasurement);
+        await this.channelManager.postToChannel(this.channelId, wbcObservation);
         this.emit('updated');
     }
 
     /**
      * returns all the wbc measurements from the channel
      */
-    async measurements(): Promise<ObjectData<WbcMeasurement>[]> {
-        const objects: ObjectData<WbcMeasurement>[] = [];
-        const oneObjects = await this.channelManager.getObjectsWithType('WbcMeasurement');
-
-        // Convert the data member from one to model representation
-        for (const oneObject of oneObjects) {
-            const {data, ...restObjectData} = oneObject;
-            objects.push({...restObjectData, data: data});
-        }
-
-        return objects;
+    async measurements(): Promise<ObjectData<WbcObservation>[]> {
+        return await this.channelManager.getObjectsWithType('WbcObservation');
     }
 
     /**
      * returns the wbc measurement with that specific id provided by the ObjectData type
      */
-    async getEntryById(id: string): Promise<ObjectData<WbcMeasurement>> {
-        const {data, ...restObjectData} = await this.channelManager.getObjectWithTypeById(
-            id,
-            'WbcMeasurement'
-        );
-        return {...restObjectData, data: data};
+    async getEntryById(id: string): Promise<ObjectData<WbcObservation>> {
+        return await this.channelManager.getObjectWithTypeById(id, 'WbcObservation');
     }
 }
