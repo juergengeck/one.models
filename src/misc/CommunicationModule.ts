51b57322fad2c6b1f549b4c7cd3288b2ef026d24
Sandru Sebastian <sandrusebastian1997@gmail.com>
2020-11-12 16:07:58 +0200

    CommunicationModule hotfix

--- src/misc/CommunicationModule.ts
+++ src/misc/CommunicationModule.ts
@@ -278,6 +278,10 @@ export default class CommunicationModule extends EventEmitter {
                         const targetKey = toByteArray(remoteInstanceKeys.publicKey);
                         const mapKey = genMapKey(sourceKey, targetKey);
 
+                        // Check if the desired connection it's with you 
+                        if(mainInstanceInfo.instanceKeys.publicKey === remoteInstanceKeys.publicKey){
+                            return;
+                        }
                         // Check if there is already a matching active connection in the unknown peer maps
                         let activeConnection = this.unknownPeerMap.get(mapKey);
                         if (this.knownPeerMap.has(mapKey)) {
