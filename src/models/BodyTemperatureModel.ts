075ac4bc141816589e349b9b90b611968c9fc30c
Dragos Pavel <dragos.pavel@corandus.com>
2021-02-12 12:55:11 +0200

    reverted body temp model

--- src/models/BodyTemperatureModel.ts
+++ src/models/BodyTemperatureModel.ts
@@ -1,6 +1,7 @@
 import EventEmitter from 'events';
 import i18nModelsInstance from '../i18n';
 import ChannelManager from './ChannelManager';
+import {BodyTemperature as OneBodyTemperature} from '@OneCoreTypes';
 
 /**
  * This represents the model of a body temperature measurement
@@ -10,6 +11,17 @@ export type BodyTemperature = {
     temperature: number;
 };
 
+function convertToOneBodyTemperature(bodyTemperature: BodyTemperature): OneBodyTemperature {
+    if (bodyTemperature.temperature > 47) {
+        throw Error(i18nModelsInstance.t('errors:bodyTemperatureModel.entryError'));
+    }
+
+    return {
+        $type$: 'BodyTemperature',
+        temperature: bodyTemperature.temperature
+    };
+}
+
 /**
  * This model implements the possibility of adding a body temperature measurement into a journal and
  * keeping track of the list of the body temperature measurements
@@ -17,54 +29,25 @@ export type BodyTemperature = {
 export default class BodyTemperatureModel extends EventEmitter {
     channelManager: ChannelManager;
     channelId: string;
-    private readonly boundOnUpdatedHandler: (id: string) => Promise<void>;
 
     constructor(channelManager: ChannelManager) {
         super();
 
         this.channelManager = channelManager;
         this.channelId = 'bodyTemperature';
-        this.boundOnUpdatedHandler = this.handleChannelUpdate.bind(this);
     }
 
-    /**
-     * Initialize this instance
-     */
-    async init(): Promise<void> {
-        await this.channelManager.createChannel(this.channelId);
-        this.channelManager.on('updated', this.boundOnUpdatedHandler);
-    }
-
-    /**
-     * Shutdown module
-     */
-    public async shutdown(): Promise<void> {
-        this.channelManager.removeListener('updated', this.boundOnUpdatedHandler);
-    }
-
-    /**
-     * Used to store a body temperature in one instance.
-     * @param {number} bodyTemperature - the body temperature measurement provided by the user.
-     * @returns {Promise<void>}
-     */
-    async addBodyTemperature(bodyTemperature: number): Promise<void> {
-        /** check if the body temperature was supplied **/
-        if (bodyTemperature === undefined || bodyTemperature === null) {
+    async addBodyTemperature(bodyTemperature: BodyTemperature): Promise<void> {
+        if (!bodyTemperature.temperature) {
             throw Error(i18nModelsInstance.t('errors:bodyTemperatureModel.notEmptyField'));
         }
 
-        /** make sure that the supplied body temperature fit the allowed range **/
-        if (bodyTemperature < 35 || bodyTemperature > 45) {
-            throw Error(i18nModelsInstance.t('errors:bodyTemperatureModel.entryError'));
-        }
-
-        /** store the body temperature in one **/
         await this.channelManager.postToChannel(
             this.channelId,
-            {$type$: 'BodyTemperature', temperature: bodyTemperature},
-            undefined,
-            Date.now()
+            convertToOneBodyTemperature(bodyTemperature)
         );
+
+        this.emit('updated');
     }
 
     async getBodyTemperatures(): Promise<BodyTemperature[]> {
@@ -80,14 +63,12 @@ export default class BodyTemperatureModel extends EventEmitter {
         return objects;
     }
 
-    /**
-     *  Handler function for the 'updated' event
-     *  @param {string} id
-     * @return {Promise<void>}
-     */
-    private async handleChannelUpdate(id: string): Promise<void> {
-        if (id === this.channelId) {
-            this.emit('updated');
-        }
+    async init(): Promise<void> {
+        await this.channelManager.createChannel(this.channelId);
+        this.channelManager.on('updated', id => {
+            if (id === this.channelId) {
+                this.emit('updated');
+            }
+        });
     }
 }
