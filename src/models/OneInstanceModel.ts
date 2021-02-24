8ef93bda080702f1cf6b5f7f9602853ea3174e20
Sandru Sebastian <sandrusebastian1997@gmail.com>
2021-02-24 13:07:23 +0200

    Added register recipes in initialiseInstance in OneInstanceModel.

--- src/models/OneInstanceModel.ts
+++ src/models/OneInstanceModel.ts
@@ -1,5 +1,5 @@
 import EventEmitter from 'events';
-import {closeInstance, initInstance} from 'one.core/lib/instance';
+import {closeInstance, initInstance, registerRecipes} from 'one.core/lib/instance';
 import Recipes from '../recipes/recipes';
 import oneModules from '../generated/oneModules';
 import {Module, SHA256Hash, VersionedObjectResult, Instance, Person} from '@OneCoreTypes';
@@ -314,6 +314,7 @@ export default class OneInstanceModel extends EventEmitter {
             });
 
             await importModules();
+            await registerRecipes(Recipes);
         }
         await this.initialisingApplication();
     }
