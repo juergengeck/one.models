f983feb7cc3870dc16e7bc648046c0a6f25559bd
Maximilian Wisgickl <wisgicklma@refinio.net>
2021-02-26 08:42:05 +0100

    Split recipes

---
+++ src/recipes/recipes-experimental.ts
@@ -0,0 +1,14 @@
+import {Recipe} from '@OneCoreTypes';
+import BodyTemperatureRecipes from './BodyTemperatureRecipe';
+import BlobRecipes from './BlobRecipes';
+import MatchingRecipes from './MatchingRecipes';
+import WbcRecipes from './WbcDiffRecipes';
+
+const RecipesExperimental: Recipe[] = [
+    ...BodyTemperatureRecipes,
+    ...BlobRecipes,
+    ...MatchingRecipes,
+    ...WbcRecipes
+];
+
+export default RecipesExperimental;
--- src/recipes/recipes-stable.ts
+++ src/recipes/recipes-stable.ts
@@ -1,5 +1,4 @@
 import {Recipe} from '@OneCoreTypes';
-import BodyTemperatureRecipes from './BodyTemperatureRecipe';
 import ChannelRecipes from './ChannelRecipes';
 import ContactRecipes from './ContactRecipes';
 import DiaryRecipes from './DiaryRecipes';
@@ -10,14 +9,10 @@ import ConsentFileRecipes from './ConsentFileRecipes';
 import SettingsRecipe from './SettingsRecipe';
 import NewsRecipes from './NewsRecipes';
 import InstancesRecipes from './InstancesRecipies';
-import BlobRecipes from './BlobRecipes';
-import MatchingRecipes from './MatchingRecipes';
-import WbcRecipes from './WbcDiffRecipes';
 import DocumentRecipes from './DocumentRecipes/DocumentRecipes';
 import ECGRecipes from './ECGRecipes';
 
-const Recipes: Recipe[] = [
-    ...BodyTemperatureRecipes,
+const RecipesStable: Recipe[] = [
     ...ChannelRecipes,
     ...ContactRecipes,
     ...NewsRecipes,
@@ -28,11 +23,8 @@ const Recipes: Recipe[] = [
     ...ConsentFileRecipes,
     ...SettingsRecipe,
     ...InstancesRecipes,
-    ...BlobRecipes,
-    ...MatchingRecipes,
-    ...WbcRecipes,
     ...DocumentRecipes,
     ...ECGRecipes
 ];
 
-export default Recipes;
+export default RecipesStable;
