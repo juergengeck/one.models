464fe9e79077ea2700e040a9077c5102b15d7008
Eduard Dan Reimer <eduard.reimer@corandus.com>
2021-03-05 14:03:00 +0200

    improved documentation, change executAsync default value to false

--- src/misc/OEvent.ts
+++ src/misc/OEvent.ts
@@ -10,52 +10,65 @@ export enum EventTypes {
 }
 
 /**
- * !OEvent is chosen as class name over Event, because the second option is reserved. <br>
  * Events handling class. Interface provides possibility to register handlers for an event and to emit it. There are 3
  * possible emit options:
  *
- * emitAndForget - Use when the emitter doesn't care about the result of the execution of the listeners handlers.<br>
- * emitAll - Use when the emitter is interested in the results of the listeners handlers execution.<br>
- * emitRace - Use when the emitter is interested only in the first settled promise from the listeners handlers.
- *<p><p>
- * ####NOTE:
- * emitAndForget & emitAll offer the possibility to execute the listeners handlers in parallel or sequentially.
- * This is configurable through the 'executeAsynchronously' optional parameter in the constructor. 'executeAsynchronously'
- * defaults to true.<br>
- * executeAsynchronously === false: If an event handler is disconnected from another event handler then the other handler
+ * - emit - Use when the emitter doesn't care about the result of the execution of the listeners handlers.
+ * - emitAll - Use when the emitter is interested in the results of the listeners handlers execution.
+ * - emitRace - Use when the emitter is interested only in the first settled promise from the listeners handlers.
+ *
+ * Executing handlers synchronously vs asynchronously:
+ * -----------------------------------------------
+ * emit & emitAll offer the possibility to execute the listeners handlers in parallel or sequentially.This is
+ * configurable through the 'executeAsynchronously' optional parameter in the constructor. 'executeAsynchronously'
+ * defaults to false.
+ * - executeAsynchronously === false: If an event handler is disconnected from another event handler then the other handler
  * will not be called if it didn't run, yet. If a new one is connected it will be executed as last event handler.<br>
- * executeAsynchronously === true: If an event handler is disconnected from another event handler then the other
+ * - executeAsynchronously === true: If an event handler is disconnected from another event handler then the other
  * handler will still be called (it already started because of being asynchronous) - If one is connected in another event
  * handler it will not be called.
- * <p><p>
- * ### Usage:
  *
- * ####Emitter class:
- * ```
- * // create an event, which emits a string value
- * const event = new OEvent<(arg:string) => void>(EventTypes.Default, true);
+ * Usage:
+ * ------
+ * ```typescript
+ *  class CoffeeMachine {
+
+ *      // Event that signals when the coffee machine is powered on / off.
+ *      // state: true when powered on, false when powered off.
+ *      public onPowerChange = new OEvent<(state: boolean) => void>();
  *
- * // emit the event
- * event.emitAll('emitted string value');
+ *      // Turns the coffee machine on
+ *      public turnOn() {
+ *          //..
+ *          this.onPowerChange.emit(true);
+ *      }
  *
- *```
+ *      // Turns the coffee machine off
+ *      public turnOff() {
+ *          //..
+ *          this.onPowerChange.emit(false);
+ *      }
+ *  }
  *
- * ####Listener class:
- * ```
- * // register handler for the event; the return value of the connect function is the unregister handler
- * const disconnect = event.connect( emittedValue => {
- *     return new Promise<void>(resolve => {
- *       setTimeout(() => {
- *          resolve();
- *      }, 1 * 100);
+ *  // Use the events provided by the class:
+ *  const coffeeMachine = new CoffeeMachine();
+ *  const disconnect = coffeeMachine.onPowerChange.connect(state => {
+ *      if (state) {
+ *          console.log('Coffee machine was turned on')
+ *      } else {
+ *          console.log('Coffee machine was turned off')
+ *      }
  *  });
- * }
  *
- * //unregister
- * disconnect()
+ *  coffeeMachine.turnOn(); // This will print 'Coffee machine was turned on'
+ *  coffeeMachine.turnOff(); // This will print 'Coffee machine was turned off'
+ *  disconnect(); // This will disconnect the connection
+ *  coffeeMachine.turnOn(); // This will print nothing
+ *  coffeeMachine.turnOff(); // This will print nothing
  * ```
  *
- */
+ * OEvent is chosen as class name over Event, because the second option is reserved.
+ **/
 export class OEvent<T extends (...arg: any) => void> {
     public onError: ((err: any) => void) | null = null;
 
@@ -68,7 +81,7 @@ export class OEvent<T extends (...arg: any) => void> {
      * @param type - defines if the emit functions will throw if no one is listening.
      * @param executeAsynchronously
      */
-    constructor(type: EventTypes = EventTypes.Default, executeAsynchronously = true) {
+    constructor(type: EventTypes = EventTypes.Default, executeAsynchronously = false) {
         this.type = type;
         this.executeAsynchronously = executeAsynchronously;
     }
@@ -140,8 +153,9 @@ export class OEvent<T extends (...arg: any) => void> {
     }
 
     /**
-     * Triggers all listeners handlers, ignores possible rejections. The handlers will be executed in parallel or
-     * sequentially based on the executeAsynchronously flag set in the constructor.
+     * Triggers all listeners handlers. In the case of rejections, it passes them to the onError callback; if the
+     * onError callback is not registered, it prints it to console.error. The handlers will be executed in parallel
+     * or sequentially based on the executeAsynchronously flag set in the constructor.
      * @param emittedValue
      */
     emit(...emittedValue: Parameters<T>): void {
