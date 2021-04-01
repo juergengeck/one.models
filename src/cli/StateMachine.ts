54abce5a1b3edf98c778838611d5d6c9e4c612f0
Eduard Dan Reimer <eduard.reimer@corandus.com>
2021-04-01 17:18:01 +0300

    improved state machine cli

--- src/cli/StateMachine.ts
+++ src/cli/StateMachine.ts
@@ -1,48 +1,41 @@
 import readline from 'readline';
 import {StateMachine} from '../misc/StateMachine';
 
-export type GeneralSMStates =
-    | 'initialized'
-    | 'not initialized'
-    | 'A'
-    | 'B'
-    | 'listening'
-    | 'not listening';
-export type GeneralSMEvents = 'init' | 'shutdown' | 'AtoB' | 'BtoA';
-type SMEvents = GeneralSMEvents | 'startListen' | 'stopListen';
+type SMStates = 'initialized' | 'not initialized' | 'A' | 'B' | 'listening' | 'not listening';
+type SMEvents = 'init' | 'shutdown' | 'AtoB' | 'BtoA' | 'startListen' | 'stopListen';
 
 /**
  * Main function. This exists to be able to use await here.
  */
 async function main(): Promise<void> {
     // Describe state machine
-    const subSMLvl2 = new StateMachine<GeneralSMStates, SMEvents>();
+    const subSMLvl2 = new StateMachine<SMStates, SMEvents>();
     subSMLvl2.addState('A');
     subSMLvl2.addState('B');
-    subSMLvl2.setInitialState('A');
+    subSMLvl2.setInitialState('A', false);
     subSMLvl2.addEvent('AtoB');
     subSMLvl2.addEvent('BtoA');
     subSMLvl2.addTransition('AtoB', 'A', 'B');
     subSMLvl2.addTransition('BtoA', 'B', 'A');
 
-    const subSMLvl1 = new StateMachine<GeneralSMStates, SMEvents>();
+    const subSMLvl1 = new StateMachine<SMStates, SMEvents>();
     subSMLvl1.addState('listening', subSMLvl2);
     subSMLvl1.addState('not listening');
-    subSMLvl1.setInitialState('not listening');
+    subSMLvl1.setInitialState('not listening', false);
     subSMLvl1.addEvent('startListen');
     subSMLvl1.addEvent('stopListen');
-    subSMLvl1.addTransition('startListen', 'not listening', 'listening', 1);
-    subSMLvl1.addTransition('stopListen', 'listening', 'not listening', 1);
+    subSMLvl1.addTransition('startListen', 'not listening', 'listening');
+    subSMLvl1.addTransition('stopListen', 'listening', 'not listening');
 
-    const sm = new StateMachine<GeneralSMStates, SMEvents>();
+    const sm = new StateMachine<SMStates, SMEvents>();
     sm.addState('initialized', subSMLvl1);
     sm.addState('not initialized');
-    sm.setInitialState('not initialized');
+    sm.setInitialState('not initialized', false);
     sm.addEvent('shutdown');
     sm.addEvent('init');
-    sm.addTransition('init', 'not initialized', 'initialized', 1);
-    sm.addTransition('shutdown', 'initialized', 'not initialized', 1);
-    sm.addTransition('shutdown', 'not initialized', 'not initialized', 1);
+    sm.addTransition('init', 'not initialized', 'initialized');
+    sm.addTransition('shutdown', 'initialized', 'not initialized');
+    sm.addTransition('shutdown', 'not initialized', 'not initialized');
 
     console.log('Localize B', sm.locateState('B'));
 
@@ -63,15 +56,15 @@ async function main(): Promise<void> {
     process.on('SIGINT', sigintHandler);
 
     sm.onEnterState(state => {
-        console.log('ENTER STATE: ' + state);
+        console.log('SM onEnterState: ' + state);
     });
     sm.onLeaveState(state => {
-        console.log('LEAVE STATE: ' + state);
+        console.log('SM onLeaveState: ' + state);
     });
 
     sm.onStateChange((oldState, newState, event) => {
         console.log(
-            'STATE CHANGE: [oldState] = ' +
+            'SM onStateChange: [oldState] = ' +
                 oldState +
                 ' [newState] = ' +
                 newState +
@@ -82,7 +75,65 @@ async function main(): Promise<void> {
 
     sm.onStatesChange((oldStates, newStates, event) => {
         console.log(
-            'STATES CHANGE: [oldState] = ' +
+            'SM onStatesChange: [oldState] = ' +
+                oldStates +
+                ' [newState] = ' +
+                newStates +
+                ' [event] = ' +
+                event
+        );
+    });
+
+    subSMLvl1.onEnterState(state => {
+        console.log('subSMLvl1 onEnterState: ' + state);
+    });
+    subSMLvl1.onLeaveState(state => {
+        console.log('subSMLvl1 onLeaveState: ' + state);
+    });
+
+    subSMLvl1.onStateChange((oldState, newState, event) => {
+        console.log(
+            'subSMLvl1 onStateChange: [oldState] = ' +
+                oldState +
+                ' [newState] = ' +
+                newState +
+                ' [event] = ' +
+                event
+        );
+    });
+
+    subSMLvl1.onStatesChange((oldStates, newStates, event) => {
+        console.log(
+            'subSMLvl1 onStatesChange: [oldState] = ' +
+                oldStates +
+                ' [newState] = ' +
+                newStates +
+                ' [event] = ' +
+                event
+        );
+    });
+
+    subSMLvl2.onEnterState(state => {
+        console.log('subSMLvl2 onEnterState: ' + state);
+    });
+    subSMLvl2.onLeaveState(state => {
+        console.log('subSMLvl2 onLeaveState: ' + state);
+    });
+
+    subSMLvl2.onStateChange((oldState, newState, event) => {
+        console.log(
+            'subSMLvl2 onStateChange: [oldState] = ' +
+                oldState +
+                ' [newState] = ' +
+                newState +
+                ' [event] = ' +
+                event
+        );
+    });
+
+    subSMLvl2.onStatesChange((oldStates, newStates, event) => {
+        console.log(
+            'subSMLvl2 onStatesChange: [oldState] = ' +
                 oldStates +
                 ' [newState] = ' +
                 newStates +
@@ -94,17 +145,17 @@ async function main(): Promise<void> {
     // Read from stdin
     for await (const line of rl) {
         console.log('====================================================');
-
+        console.log('Triggered event: ' + line);
         //console.log('StateMachine: ', sm);
         sm.triggerEvent(<SMEvents>line);
         console.log('1. sm Current STATE:', sm.currentState);
         console.log('1. sm Current States: ', sm.currentStates);
-
-        console.log('2. subSMLvl1 Current STATE:', subSMLvl1.currentState);
-        console.log('2. subSMLvl1 Current States: ', subSMLvl1.currentStates);
-
-        console.log('3. subSMLvl2 Current STATE:', subSMLvl2.currentState);
-        console.log('3. subSMLvl2 Current States: ', subSMLvl2.currentStates);
+        //
+        // console.log('2. subSMLvl1 Current STATE:', subSMLvl1.currentState);
+        // console.log('2. subSMLvl1 Current States: ', subSMLvl1.currentStates);
+        //
+        // console.log('3. subSMLvl2 Current STATE:', subSMLvl2.currentState);
+        // console.log('3. subSMLvl2 Current States: ', subSMLvl2.currentStates);
     }
 }
 
