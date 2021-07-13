51dab64f6005943aeedba420adc52096f7e02a9b
Michael Hasenstein <hasenstein@yahoo.com>
2021-07-13 11:53:30 +0200

    Restore the removed ConsentFile-model files and fix TS issues

---
+++ src/recipes/ConsentFileRecipes.ts
@@ -0,0 +1,32 @@
+import type {Recipe} from 'one.core/lib/recipes';
+
+declare module '@OneObjectInterfaces' {
+    export interface OneUnversionedObjectInterfaces {
+        ConsentFile: ConsentFile;
+    }
+}
+
+export interface ConsentFile {
+    $type$: 'ConsentFile';
+    fileData: string;
+    fileType: string;
+}
+
+const ConsentFileRecipe: Recipe = {
+    $type$: 'Recipe',
+    name: 'ConsentFile',
+    rule: [
+        {
+            itemprop: 'fileData',
+            valueType: 'string'
+        },
+        {
+            itemprop: 'fileType',
+            valueType: 'string'
+        }
+    ]
+};
+
+const ConsentFile: Recipe[] = [ConsentFileRecipe];
+
+export default ConsentFile;
