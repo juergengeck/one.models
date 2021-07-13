28043ff67de1d9c17ea0d2d4493381b02b5f894f
Michael Hasenstein <hasenstein@yahoo.com>
2021-07-13 16:41:58 +0200

    REVERT JournalModel changes

--- src/models/JournalModel.ts
+++ src/models/JournalModel.ts
@@ -6,38 +6,90 @@ import EventEmitter from 'events';
 import type {ObjectData, QueryOptions} from './ChannelManager';
 import {OEvent} from '../misc/OEvent';
 import type {Model} from './Model';
+import type {WbcObservation} from '../recipes/WbcDiffRecipes';
+import type {QuestionnaireResponses} from '../recipes/QuestionnaireRecipes/QuestionnaireResponseRecipes';
+import type {DocumentInfo_1_1_0} from '../recipes/DocumentRecipes/DocumentRecipes_1_1_0';
+import type {DiaryEntry} from '../recipes/DiaryRecipes';
+import type {ConsentFile} from '../recipes/ConsentFileRecipes';
+import type {Electrocardiogram} from '../recipes/ECGRecipes';
+import type {BodyTemperature} from '../recipes/BodyTemperatureRecipe';
+import type {HeartEvent} from '../recipes/HeartEventRecipes';
+import type HeartEventModel from './HeartEventModel';
+import type WbcDiffModel from './WbcDiffModel';
+import type QuestionnaireModel from './QuestionnaireModel';
+import type DocumentModel from './DocumentModel';
+import type DiaryModel from './DiaryModel';
+import type ConsentFileModel from './ConsentFileModel';
+import type ECGModel from './ECGModel';
+import type BodyTemperatureModel from './BodyTemperatureModel';
+import type {OneUnversionedObjectTypes} from 'one.core/lib/recipes';
 
-export type JournalEntry = {
-    type: string;
-    data: ObjectData<unknown>;
+/**
+ * !!! Add the corresponding model class name here
+ */
+export enum EventType {
+    QuestionnaireResponse = 'QuestionnaireModel',
+    WbcDiffMeasurement = 'WbcDiffModel',
+    HeartEvent = 'HeartEventModel',
+    DocumentInfo = 'DocumentModel',
+    DiaryEntry = 'DiaryModel',
+    BodyTemperature = 'BodyTemperatureModel',
+    ConsentFileEvent = 'ConsentFileModel',
+    ECGEvent = 'ECGModel'
+}
+
+/**
+ * Add the corresponding type here
+ */
+export type EventListEntry = {
+    type: EventType;
+    data: ObjectData<
+        | WbcObservation
+        | QuestionnaireResponses
+        | DocumentInfo_1_1_0
+        | DiaryEntry
+        | ConsentFile
+        | Electrocardiogram
+        | BodyTemperature
+        | HeartEvent
+    >;
 };
 
 type JournalInput = {
-    model: Model;
+    model:
+        | HeartEventModel
+        | WbcDiffModel
+        | QuestionnaireModel
+        | DocumentModel
+        | DiaryModel
+        | ConsentFileModel
+        | ECGModel
+        | BodyTemperatureModel;
     retrieveFn: (
         queryOptions?: QueryOptions
-    ) => AsyncIterableIterator<ObjectData<unknown> | Promise<ObjectData<unknown>>>;
-    eventType: string;
+    ) => AsyncIterableIterator<EventListEntry['data'] | Promise<EventListEntry['data']>>;
+    eventType: EventType;
 };
 
 type TimeFrame = {from: Date; to: Date};
 
-type JournalData = {[event: string]: {values: ObjectData<unknown>[]; index: number}};
+type JournalData = {[event: string]: {values: EventListEntry['data'][]; index: number}};
 
 const ONE_DAY_MS = 1000 * 60 * 60 * 24;
 
-export default class JournalModel extends EventEmitter implements Model {
-    private readonly modelsDictionary: JournalInput[];
+export default class JournalModel extends EventEmitter {
+    private modelsDictionary: JournalInput[] = [];
 
+    private eventEmitterListeners: Map<EventType, () => void> = new Map();
     private oEventListeners: Map<
-        string,
+        EventType,
         {
             disconnect: (() => void) | undefined;
-            listener: (data: ObjectData<unknown>) => void;
+            listener: (data?: ObjectData<OneUnversionedObjectTypes>) => void;
         }
     > = new Map();
 
-    public onUpdated = new OEvent<(data: ObjectData<unknown>, type: string) => void>();
+    public onUpdated = new OEvent<(data?: EventListEntry) => void>();
 
     constructor(modelsInput: JournalInput[]) {
         super();
@@ -51,11 +103,22 @@ export default class JournalModel extends EventEmitter implements Model {
     async init() {
         this.modelsDictionary.forEach((journalInput: JournalInput) => {
             const event = journalInput.eventType;
-            const oEventHandler = (data: ObjectData<unknown>) => {
-                this.onUpdated.emit(data, event);
+            /*
+             * @Todo this event will be removed in the future for the only use of oEvent
+             */
+            const handlerEventEmitter = () => {
+                this.emit('updated');
             };
+            const oEventHandler = (data?: ObjectData<OneUnversionedObjectTypes>) => {
+                /** It's guaranteed that incoming objects are "data" of {@link EventListEntry} because of the {@link JournalInput} **/
+                this.onUpdated.emit(
+                    JournalModel.mapObjectDataToEventListEntry(data as EventListEntry['data'])
+                );
+            };
+            journalInput.model.on('updated', handlerEventEmitter);
             const disconnectFn = journalInput.model.onUpdated(oEventHandler.bind(this));
             /** persist the function reference in a map **/
+            this.eventEmitterListeners.set(event, handlerEventEmitter);
             this.oEventListeners.set(event, {listener: oEventHandler, disconnect: disconnectFn});
         });
     }
@@ -63,12 +126,19 @@ export default class JournalModel extends EventEmitter implements Model {
     /**
      * removes the handler for every provided model
      */
-    async shutdown(): Promise<void> {
+    shutdown() {
         this.modelsDictionary.forEach((journalInput: JournalInput) => {
-            const oEventHandler = this.oEventListeners.get(journalInput.eventType);
+            const event = journalInput.eventType;
+            /** retrieve the function reference in order to delete it **/
+            const eventEmitterHandler = this.eventEmitterListeners.get(event);
+            const oEventHandler = this.oEventListeners.get(event);
 
             if (oEventHandler && oEventHandler.disconnect) {
                 oEventHandler.disconnect();
+            }
+
+            if (eventEmitterHandler) {
+                journalInput.model.removeListener('updated', eventEmitterHandler);
             }
         });
     }
@@ -76,7 +146,7 @@ export default class JournalModel extends EventEmitter implements Model {
     /**
      * Get the latest day stored events sorted by date. In Ascending order
      */
-    async retrieveLatestDayEvents(): Promise<JournalEntry[]> {
+    async retrieveLatestDayEvents(): Promise<EventListEntry[]> {
         /** if there are no provided models, return empty list **/
         if (this.modelsDictionary.length === 0) {
             return [];
@@ -89,12 +159,12 @@ export default class JournalModel extends EventEmitter implements Model {
         await Promise.all(
             this.modelsDictionary.map(async (journalInput: JournalInput) => {
                 const event = journalInput.eventType;
-                const data: ObjectData<unknown>[] = [];
+                const data: EventListEntry['data'][] = [];
                 for await (const retrievedData of journalInput.retrieveFn({
                     to: latestTo,
                     from: latestFrom
                 })) {
-                    data.push(await retrievedData);
+                    data.push(retrievedData as unknown as EventListEntry['data']);
                 }
                 dataDictionary[event] = {
                     values: data,
@@ -110,7 +180,7 @@ export default class JournalModel extends EventEmitter implements Model {
      */
     async *retrieveEventsByDayIterator(
         pageSize: number = 25
-    ): AsyncIterableIterator<JournalEntry[]> {
+    ): AsyncIterableIterator<EventListEntry[]> {
         /**
          * Find the highest timestamp and set the currentTimeFrame to it.
          * The "from" field will be one day behind the "to" field.
@@ -154,7 +224,8 @@ export default class JournalModel extends EventEmitter implements Model {
                         counter = 0;
                     }
 
-                    const data = await retrievedData;
+                    /** same issue as in {@link this.findLatestTimeFrame} **/
+                    const data = retrievedData as EventListEntry['data'];
 
                     /** If the event exists in the dictionary and if the array exists,
                      *  create a new array with the new value and the rest of the array
@@ -193,9 +264,9 @@ export default class JournalModel extends EventEmitter implements Model {
 
     /**
      * Get the stored events sorted by date. In Ascending order
-     * @returns {Promise<JournalEntry[]>}
+     * @returns {Promise<EventListEntry[]>}
      */
-    async retrieveAllEvents(): Promise<JournalEntry[]> {
+    async retrieveAllEvents(): Promise<EventListEntry[]> {
         /** if there are no provided models, return empty list **/
         if (this.modelsDictionary.length === 0) {
             return [];
@@ -206,9 +277,9 @@ export default class JournalModel extends EventEmitter implements Model {
         await Promise.all(
             this.modelsDictionary.map(async (journalInput: JournalInput) => {
                 const event = journalInput.eventType;
-                const data: ObjectData<unknown>[] = [];
+                const data: EventListEntry['data'][] = [];
                 for await (const retrievedData of journalInput.retrieveFn()) {
-                    data.push(await retrievedData);
+                    data.push(retrievedData as unknown as EventListEntry['data']);
                 }
                 dataDictionary[event] = {
                     values: data,
@@ -224,16 +295,16 @@ export default class JournalModel extends EventEmitter implements Model {
      * @param {JournalData} dataDictionary
      * @private
      */
-    private createEventList(dataDictionary: JournalData): JournalEntry[] {
+    private createEventList(dataDictionary: JournalData): EventListEntry[] {
         /** get the total length of data values **/
         const totalLen: number = Object.keys(dataDictionary)
             .map((event: string) => dataDictionary[event].values.length)
             .reduce((acc: number, cur: number) => acc + cur);
 
-        const eventList = [];
+        const eventList: EventListEntry[] = [];
 
         for (let i = 0; i < totalLen; ++i) {
-            const compareElements = [];
+            const compareElements: EventListEntry[] = [];
             /** for every key of the data dictionary **/
             for (const event of Object.keys(dataDictionary)) {
                 /** get the actual object **/
@@ -242,7 +313,7 @@ export default class JournalModel extends EventEmitter implements Model {
                 if (eventData.index < eventData.values.length) {
                     compareElements.push({
                         /** put the data key as the event type, also = model class name **/
-                        type: event,
+                        type: event as EventType,
                         data: eventData.values[eventData.index]
                     });
                 }
@@ -256,7 +327,7 @@ export default class JournalModel extends EventEmitter implements Model {
                 );
             }
             /** Lets find the element with the newest date **/
-            let oldestElement = compareElements[0];
+            let oldestElement: EventListEntry = compareElements[0];
 
             for (const compareElement of compareElements) {
                 if (compareElement.data.creationTime < oldestElement.data.creationTime) {
@@ -282,13 +353,16 @@ export default class JournalModel extends EventEmitter implements Model {
     private async findLatestTimeFrame(from?: Date, to?: Date): Promise<number> {
         const timestamps = await Promise.all(
             this.modelsDictionary.map(async (journalInput: JournalInput) => {
-                let data: ObjectData<unknown> | null = null;
+                let data: EventListEntry['data'] | null = null;
                 for await (const retrievedData of journalInput.retrieveFn({
                     count: 1,
                     to: to,
                     from: from
                 })) {
-                    data = await retrievedData;
+                    /** The return type of provided functions will always be EventListEntry['data'].
+                     *  I don't know the reason why TS does not recognise the return type of the retrieve function.
+                     **/
+                    data = retrievedData as EventListEntry['data'];
                 }
                 if (data !== null) {
                     return data.creationTime.getTime();
@@ -297,5 +371,38 @@ export default class JournalModel extends EventEmitter implements Model {
             })
         );
         return Math.max(...timestamps);
+    }
+
+    /**
+     * Maps the given object data to the corresponding event type
+     * @param {ObjectData<OneUnversionedObjectTypes>} objectData
+     * @private
+     */
+    private static mapObjectDataToEventListEntry(
+        objectData?: EventListEntry['data']
+    ): EventListEntry | undefined {
+        if (!objectData) {
+            return undefined;
+        }
+
+        switch (objectData.data.$type$) {
+            case 'ConsentFile':
+                return {type: EventType.ConsentFileEvent, data: objectData};
+            case 'DocumentInfo_1_1_0':
+                return {type: EventType.DocumentInfo, data: objectData};
+            case 'WbcObservation':
+                return {type: EventType.WbcDiffMeasurement, data: objectData};
+            case 'BodyTemperature':
+                return {type: EventType.BodyTemperature, data: objectData};
+            case 'QuestionnaireResponses':
+                return {type: EventType.QuestionnaireResponse, data: objectData};
+            case 'DiaryEntry':
+                return {type: EventType.DiaryEntry, data: objectData};
+            case 'Electrocardiogram':
+                return {type: EventType.ECGEvent, data: objectData};
+            case 'HeartEvent':
+                return {type: EventType.HeartEvent, data: objectData};
+        }
+        return undefined;
     }
 }
