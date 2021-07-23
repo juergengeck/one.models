6b4a34648d335d798f9283f391b179564225b974
edreimer <72192800+edreimer@users.noreply.github.com>
2021-07-23 14:32:07 +0300

    Update install.js

--- install.js
+++ install.js
@@ -30,6 +30,8 @@ const main = () => {
     exec(install, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
             return;
         }
         console.log(`stdout: ${stdout}`);
