99853fd995ce231851e42f0b6283eb4cace2b458
Maximilian Kallert <kallertma@protonmail.com>
2021-07-29 10:06:16 +0200

    Reduce compiler options

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         filename: 'commServer.bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
