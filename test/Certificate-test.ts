6ad1d5bd825ee9bcc87418d716ae806560e9a508
Sandru Sebastian <sandrusebastian1997@gmail.com>
2021-10-25 14:52:40 +0300

    Implemented Certificate Manager.

--- test/Certificate-test.ts
+++ test/Certificate-test.ts
@@ -12,7 +12,7 @@ import type {Keys, OneUnversionedObjectTypes, Person} from 'one.core/lib/recipes
 import { expect } from 'chai';
 import {initLicenses} from '../lib/misc/License';
 import {revokeCertificate} from '../lib/misc/Certificate';
-import {findCertificatesForObject, findObjectsSharedWithPerson} from '../lib/misc/Certificate';
+import {CertificateManager} from '../lib/models';
 
 describe('Certificate test', () => {
     const instancesModel = new InstancesModel();
@@ -124,13 +124,19 @@ describe('Certificate test', () => {
         });
     })
     it('Should find certificates for a specific object', async () => {
+        const certificateManager = new CertificateManager(leuteModel);
+        await certificateManager.init();
+
         const cert = await createCertificate('access', subject, issuer, target);
-        const targets = await findCertificatesForObject(subject);
+        const targets = await certificateManager.findWithWhomTheObjectWasSharedByValidCertificate(subject);
         expect(targets).to.deep.equal([target]);
     });
     it('Should find shared objects with a person', async () => {
+        const certificateManager = new CertificateManager(leuteModel);
+        await certificateManager.init();
+
         const cert = await createCertificate('access', subject, issuer, target);
-        const subjects = await findObjectsSharedWithPerson(target);
+        const subjects = await certificateManager.findWhatObjectsPersonHasThoughValidCertificate(target);
         expect(subjects).to.deep.equal([subject]);
     });
 });
