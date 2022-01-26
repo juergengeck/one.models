646f6d06c2b295f6170d4ff29e708253f4020d6a
Erik Haßlmeyer <erik@refinio.net>
2022-01-26 11:24:29 +0100

    changed CommAll script

--- src/tools/CommunicationAll.ts
+++ src/tools/CommunicationAll.ts
@@ -9,20 +9,25 @@
  * other instances import it and then know how to contact this instance.
  *
  * How you use this script:
- * Step 1: You call this script in multiple terminals with different instance names:
- *     Terminal 1: node ./lib/tools/CommunicationAll -i i1
- *     Terminal 2: node ./lib/tools/CommunicationAll -i i2
+ * Step 1: You have to start a communication server that relays all information
+ *     Terminal 1: node ./lib/tools/CommunicationServer
+ * Step 2: You call this script in multiple terminals with different instance names:
+ *     Terminal 2: node ./lib/tools/CommunicationAll -i i1
+ *     Terminal 3: node ./lib/tools/CommunicationAll -i i2
  *     ...
  *
- *     This will write a file with <name>.id.json
- * Step 2:
+ *     This will write a file with <name>.id.json for each instance, so
+ *     - i1.is.json
+ *     - i2.id.json
+ *     ...
+ * Step 3:
  *     After all terminals prompt you with
  *     "Press a key to continue when you started all other processes ..."
  *     you press a key in each terminal
  *
  *     This will import all *.id.json files in the current process and the app will start making
  *     connections to all others.
- * Step 3:
+ * Step 4:
  *     The app will now print every 3 seconds the number of established connections. If you kill
  *     one or more instances the count for the others goes down. If you restart them, the count
  *     goes up again.
@@ -32,7 +37,12 @@ import yargs from 'yargs';
 import * as Logger from '@refinio/one.core/lib/logger';
 import {AccessModel, ChannelManager, ConnectionsModel, LeuteModel} from '../models';
 import InstancesModel from '../models/InstancesModel';
-import {closeInstance, initInstance} from '@refinio/one.core/lib/instance';
+import {
+    closeInstance,
+    getInstanceIdHash,
+    getInstanceOwnerIdHash,
+    initInstance
+} from '@refinio/one.core/lib/instance';
 import RecipesStable from '../recipes/recipes-stable';
 import RecipesExperimental from '../recipes/recipes-experimental';
 import oneModules from '../generated/oneModules';
@@ -67,9 +77,9 @@ function parseCommandLine(): {
             .demandOption('i')
 
             // Display connection details
-            .describe('d', 'Display connection details')
-            .boolean('d')
-            .default('d', false)
+            .describe('c', 'Display connection details')
+            .boolean('c')
+            .default('c', false)
 
             // Logger
             .describe('l', 'Enable logger')
@@ -90,7 +100,7 @@ function parseCommandLine(): {
     return {
         commServerUrl: argv.u,
         instanceName: argv.i,
-        displayDetails: argv.d === undefined ? false : argv.d
+        displayDetails: argv.c
     };
 }
 
@@ -172,13 +182,48 @@ async function setupOneModels(commServerUrl: string): Promise<{
 }
 
 /**
- * Main function. This exists to be able to use await here.
+ * Initialize everything (one.core and one.models) and exchange identities with other instances.
+ *
+ * The identity exchange between instances is done by files that contain all the necessary
+ * information to establish a connection between instances.
+ *
+ * The detailed workflow is this:
+ * 1) This function will write an instanceName.id.json file that contains the contact information
+ *    of this instance
+ * 2) Everything (one.core & one.models) will be initialized
+ * 3) The process waits for the user to press a key. Before pressing the key the user needs to
+ *    spin up the other instance processes, so that they can write their *.id.json files (they
+ *    need a different instanceName !)
+ * 4) After the user presses a key all other .id.json files will be imported into this instance
+ *    As a result connections will automatically be opened to all other instances.
+ *
+ * Notes:
+ * - The connections will be always established to all known other instances as soon as the
+ *   connectionsModel is initialized. There is no way to turn off individual connections at the
+ *   moment (future feature). The only way to shut down connections is to shutdown the connections
+ *   model.
+ * - In the default connection model configuration the connection between two instances will only
+ *   work if both instances know each other. Connection attempts from unknown instances are refused.
+ * - At the moment everything is relayed through a comm server, because browsers cannot open
+ *   ports for incoming connections. For node processes we have the necessary code to accept
+ *   incoming connections, but we don't have a configuration flag to enable this, yet.
+ *
+ * @param commServerUrl - The url of the commserver to use.
+ * @param instanceName - The name of the instance to use. This needs to be unique between all
+ *                       connected instances, because the written identity files uses this
+ *                       string as file name.
  */
-async function main(): Promise<void> {
-    // ######## Initialization / Setup ########
-
-    const {commServerUrl, instanceName, displayDetails} = parseCommandLine();
-
+async function initWithIdentityExchange(
+    commServerUrl: string,
+    instanceName: string
+): Promise<{
+    access: AccessModel;
+    instances: InstancesModel;
+    channelManager: ChannelManager;
+    leute: LeuteModel;
+    connections: ConnectionsModel;
+    shutdown: () => Promise<void>;
+}> {
     // ######## Step 1: Create own identity (if not alread done) and setup app ########
     const identity = await readOrCreateIdentityFile(
         `${instanceName}.id.json`,
@@ -195,13 +240,9 @@ async function main(): Promise<void> {
     // Setup shutdown
     async function shutdown(): Promise<void> {
         console.log('Shutting down application');
-        if (intervalHandle !== undefined) {
-            clearInterval(intervalHandle);
-        }
         await oneModels.shutdown();
         await oneCore.shutdown();
     }
-    process.on('SIGINT', () => shutdown().catch(console.error));
 
     // ######## Step 2: Import other identities ########
 
@@ -211,14 +252,36 @@ async function main(): Promise<void> {
     const profiles = await importIdentityFilesAsProfiles(`${instanceName}.id.json`);
     console.log(`Imported ${profiles.length} profiles.`);
 
-    // ######## Step 3: Print connection information each 3 seconds ########
+    return {
+        ...oneModels,
+        shutdown
+    };
+}
+
+/**
+ * Main function. This exists to be able to use await here.
+ *
+ * This main function just establishes connections to multiple instances and prints the
+ * connection details every three seconds, so that you can see the number of connections that
+ * are currently established.
+ */
+async function main(): Promise<void> {
+    // CLI options parsing & init & shutdown on SIGINT
+    const {commServerUrl, instanceName, displayDetails} = parseCommandLine();
+    const models = await initWithIdentityExchange(commServerUrl, instanceName);
+    process.on('SIGINT', () => {
+        clearInterval(intervalHandle);
+        models.shutdown().catch(console.error);
+    });
 
     // Print connection status continuously
+    console.log('Owner:', getInstanceOwnerIdHash());
+    console.log('Instance:', getInstanceIdHash());
     const intervalHandle = setInterval(() => {
-        const connectionInfos = oneModels.connections.connectionsInfo();
+        const connectionInfos = models.connections.connectionsInfo();
         const connCount = connectionInfos.filter(info => info.isConnected).length;
         connectionInfos.map(info => info.isConnected);
-        console.log(`OnelineState: ${oneModels.connections.onlineState}`);
+        console.log(`OnelineState: ${models.connections.onlineState}`);
         console.log(`Connections established: ${connCount}`);
         if (displayDetails) {
             console.log(connectionInfos);
