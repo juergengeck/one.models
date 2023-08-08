8bf131278b625ae0d9591c4c18a9fd51e994ae63
Simeon-Banov-Dev <simeon.banov@modularity.bg>
2023-08-08 15:40:07 +0300

    add disabledDisplay as new QuestionnaireRecipe version 2_1_0

---
+++ src/recipes/QuestionnaireRecipes/QuestionnaireRecipes_2_1_0.ts
@@ -0,0 +1,118 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable no-redeclare */
+import type {Recipe, RecipeRule} from '@refinio/one.core/lib/recipes';
+import {
+    CodingRules,
+    QuestionnaireRules as QuestionnaireRules_2_0_0
+} from './QuestionnaireRecipes_2_0_0';
+import type {Questionnaire_2_0_0} from './QuestionnaireRecipes_2_0_0';
+import {addRule, cloneRule, overwriteRule} from '../RecipeUtils';
+
+declare module '@OneObjectInterfaces' {
+    export interface OneUnversionedObjectInterfaces {
+        Questionnaire_2_1_0: Questionnaire_2_1_0;
+    }
+}
+/**
+ * FHIR Questionnaire type
+ */
+export interface Questionnaire_2_1_0 extends Omit<Questionnaire_2_0_0, '$type$' | 'item'> {
+    $type$: 'Questionnaire_2_1_0';
+    item: Questionnaire_2_1_0.Question[];
+}
+
+export module Questionnaire_2_1_0 {
+    /**
+     * Question of a questionnaire.
+     */
+    export type Question = Omit<Questionnaire_2_0_0.Question, 'item'> & {
+        disabledDisplay: QuestionnaireItemDisabledDisplay;
+        item?: Question[];
+    };
+
+    export type Coding = Questionnaire_2_0_0.Coding;
+    export type QuestionnaireEnableWhenAnswer = Questionnaire_2_0_0.QuestionnaireEnableWhenAnswer;
+    export type QuestionnaireAnswerOptionValue = Questionnaire_2_0_0.QuestionnaireAnswerOptionValue;
+    export type QuestionnaireValue = Questionnaire_2_0_0.QuestionnaireValue;
+    export type Attachment = Questionnaire_2_0_0.Attachment;
+    export type ExtensionMinValue = Questionnaire_2_0_0.ExtensionMinValue;
+    export type ExtensionMaxValue = Questionnaire_2_0_0.ExtensionMaxValue;
+    export type ExtensionEntryFormat = Questionnaire_2_0_0.ExtensionEntryFormat;
+    export type ExtensionMinLength = Questionnaire_2_0_0.ExtensionMinLength;
+    export type ExtensionDesignNote = Questionnaire_2_0_0.ExtensionDesignNote;
+    export type ExtensionRegEx = Questionnaire_2_0_0.ExtensionRegEx;
+    export type Extension = Questionnaire_2_0_0.Extension;
+
+    /**
+     * Type of questionnaire answers and initial values.
+     */
+    export type QuestionnaireItemDisabledDisplay =
+        | {
+              system: 'http://hl7.org/fhir/ValueSet/questionnaire-disabled-display';
+              version?: '5.0.0';
+              code: 'hidden';
+              display?: 'Hidden';
+          }
+        | {
+              system: 'http://hl7.org/fhir/ValueSet/questionnaire-disabled-display';
+              version?: '5.0.0';
+              code: 'protected';
+              display?: 'Protected';
+          };
+}
+
+/**
+ * Values for disabledDisplay on answers
+ */
+export const QuestionnaireDisabledDisplayRule: RecipeRule[] = [
+    {
+        itemprop: 'system',
+        itemtype: {type: 'string'},
+        optional: false
+    },
+    {
+        itemprop: 'version',
+        itemtype: {type: 'string'},
+        optional: true
+    },
+    {
+        itemprop: 'code',
+        itemtype: {type: 'string'},
+        optional: false
+    },
+    {
+        itemprop: 'display',
+        itemtype: {type: 'string'},
+        optional: true
+    }
+];
+
+/**
+ * The rules to build a questionnaire based on FHIR
+ */
+export const QuestionnaireRules: RecipeRule[] = cloneRule(QuestionnaireRules_2_0_0);
+overwriteRule(QuestionnaireRules, 'item', {
+    itemprop: 'item',
+    inheritFrom: 'Questionnaire_2_1_0.item',
+    optional: true
+});
+
+addRule(QuestionnaireRules, 'item', {
+    itemprop: 'disabledDisplay',
+    itemtype: {type: 'object', rules: QuestionnaireDisabledDisplayRule},
+    optional: true
+});
+
+/**
+ * Recipe for questionnaires based upon FHIR standard.
+ *
+ * @type {{name: string; rule: RecipeRule[]; $type$: string}}
+ */
+export const QuestionnaireRecipe: Recipe = {
+    $type$: 'Recipe',
+    name: 'Questionnaire_2_1_0',
+    rule: QuestionnaireRules
+};
+
+const QuestionnaireRecipes: Recipe[] = [QuestionnaireRecipe];
+export default QuestionnaireRecipes;
