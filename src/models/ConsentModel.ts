bba8a32ae524f8040f6704d98016826b2fab9cc1
Simeon-Banov-Dev <simeon.banov@modularity.bg>
2023-08-15 15:02:38 +0300

    export recipe version from model as Consent, regardless of version

--- src/models/ConsentModel.ts
+++ src/models/ConsentModel.ts
@@ -9,6 +9,8 @@ import {
 import {sign} from '../misc/Signature';
 import {storeFileWithBlobDescriptor} from '../misc/storeFileWithBlobDescriptor';
 
+export type Consent = Consent_1_1_0;
+
 type FileStatusTuple = [File, Consent_1_1_0['status']];
 type TextStatusTuple = [string, Consent_1_1_0['status']];
 
