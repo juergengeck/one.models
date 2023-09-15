b41851c8a9e41c7ed9c96df0bf39024ce77783cb
MIchael Hasenstein <hasenstein@yahoo.com>
2023-09-15 09:23:52 +0200

    Remove misc/fetchFile.ts, which has been moved to one.core, and replace the two uses of the two functions in that module by changing the imports to the two new modules in the new one.core

--- src/misc/identityExchange-browser.ts
+++ src/misc/identityExchange-browser.ts
@@ -1,8 +1,9 @@
+import type {UnversionedObjectResult} from '@refinio/one.core/lib/storage-unversioned-objects.js';
+import {fetchFile} from '@refinio/one.core/lib/system/fetch-file.js';
+
 import type {Identity} from './IdentityExchange.js';
 import {convertIdentityToOneInstanceEndpoint, isIdentity} from './IdentityExchange.js';
 import type {OneInstanceEndpoint} from '../recipes/Leute/CommunicationEndpoints.js';
-import {fetchFile} from './fetchFile.js';
-import type {UnversionedObjectResult} from '@refinio/one.core/lib/storage-unversioned-objects.js';
 
 // ######## Identity I/O ########
 
